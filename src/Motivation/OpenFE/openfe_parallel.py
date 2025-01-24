903690a528b88082b06445be3a0cf88b8322c38e
bastis007 <schaefer.bastian@mail.de>
2025-01-24 14:51:56 +0100

    Add OpenFE libraries + folder

--- src/Motivation/OpenFE/openfe_parallel.py
+++ src/Motivation/OpenFE/openfe_parallel.py
@@ -1,14 +1,13 @@
 import gc
 import os
-import warnings
 import lightgbm as lgb
 import pandas as pd
 from sklearn.model_selection import train_test_split, StratifiedKFold, KFold
-from .FeatureGenerator import *
+from FeatureGenerator import *
 import random
 from concurrent.futures import ProcessPoolExecutor
 import traceback
-from .utils import tree_to_formula, check_xor, formula_to_tree
+from utils import tree_to_formula, check_xor, formula_to_tree
 from sklearn.inspection import permutation_importance
 from sklearn.feature_selection import mutual_info_regression, mutual_info_classif
 from sklearn.metrics import mean_squared_error, log_loss, roc_auc_score
