7e0e8ca542e64711cb470becab1fb33e400c7ab7
bastis007 <schaefer.bastian@mail.de>
2025-01-27 11:20:45 +0100

    Update writing and remove files in the end

--- src/Motivation/Collect_Results.py
+++ src/Motivation/Collect_Results.py
@@ -1,8 +1,14 @@
 from glob import glob
+import os
 
-files = glob('*results_*.txt')
+files = glob('../../results_*.txt')
+print(files)
+
+with open('../../results.txt', 'w') as results_file:
+    results_file.write("Collected Results\n")
 for file in files:
     with open(file, 'r') as f:
         content = f.read()
-        with open('results.txt', 'a') as results_file:
+        with open('../../results.txt', 'a') as results_file:
             results_file.write(content)
+    os.remove(file)
