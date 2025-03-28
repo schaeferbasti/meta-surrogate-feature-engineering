2e13fb5ee7c457e98a915aa7d1183348b87fc3ad
bastis007 <schaefer.bastian@mail.de>
2025-03-28 10:59:11 +0100

    Fix circular import problems

--- src/SurrogateModel/SurrogateModel.py
+++ src/SurrogateModel/SurrogateModel.py
@@ -6,7 +6,7 @@ from src.utils.get_dataset import get_openml_dataset_and_metadata
 from src.utils.get_matrix import get_matrix_columns
 from src.utils.get_metafeatures import get_numeric_pandas_metafeatures, get_categorical_pandas_metafeatures
 from src.utils.preprocess_data import factorize_data
-from src.utils.run_autogluon import multi_predict_operators_for_models, predict_operators_for_models
+from src.utils.run_models import multi_predict_operators_for_models, predict_operators_for_models
 
 
 def add_metadata(X_predict, dataset_metadata, models):
@@ -24,33 +24,6 @@ def add_metadata(X_predict, dataset_metadata, models):
                 feature_metadata_categorical = get_categorical_pandas_metafeatures(feature_df, featurename)
                 X_predict_new.loc[len(X_predict_new)] = [dataset_metadata["task_id"], dataset_metadata["task_type"], dataset_metadata["number_of_classes"], feature_metadata_categorical["feature - name"], "operator", str(feature_datatype), int(feature_metadata_categorical["feature - count"]), None, None, None, None, None, None, None, feature_metadata_categorical["feature - unique"], feature_metadata_categorical["feature - top"], feature_metadata_categorical["feature - freq"], model, None]
     return X_predict_new
-
-
-def add_new_featurenames(X_test):
-    # Get new dataset with feature names and metafeatures and replicate each feature (=each row) x times, that we can repeat thus row with similar values, but instead of the feature name, we add a new name consisting of all available operators and respective features
-    matrix_columns = get_matrix_columns()
-    featurenames = create_featurenames(X_test["feature - name"].values)
-    X_test_new = pd.DataFrame({
-        matrix_columns[0]: np.repeat((data := X_test[matrix_columns[0]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
-        matrix_columns[1]: np.repeat((data := X_test[matrix_columns[1]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
-        matrix_columns[2]: np.repeat((data := X_test[matrix_columns[2]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
-        matrix_columns[3]: np.repeat(np.array(featurenames), data.shape[0]).reshape(len(featurenames), data.shape[0]).T.flatten(),
-        matrix_columns[4]: np.repeat((data := X_test[matrix_columns[4]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
-        matrix_columns[5]: np.repeat((data := X_test[matrix_columns[5]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
-        matrix_columns[6]: np.repeat((data := X_test[matrix_columns[6]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
-        matrix_columns[7]: np.repeat((data := X_test[matrix_columns[7]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
-        matrix_columns[8]: np.repeat((data := X_test[matrix_columns[8]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
-        matrix_columns[9]: np.repeat((data := X_test[matrix_columns[9]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
-        matrix_columns[10]: np.repeat((data := X_test[matrix_columns[10]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
-        matrix_columns[11]: np.repeat((data := X_test[matrix_columns[11]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
-        matrix_columns[12]: np.repeat((data := X_test[matrix_columns[12]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
-        matrix_columns[13]: np.repeat((data := X_test[matrix_columns[13]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
-        matrix_columns[14]: np.repeat((data := X_test[matrix_columns[14]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
-        matrix_columns[15]: np.repeat((data := X_test[matrix_columns[15]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
-        matrix_columns[16]: np.repeat((data := X_test[matrix_columns[16]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
-        matrix_columns[17]: np.repeat((data := X_test[matrix_columns[17]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
-    })
-    return X_test_new
 
 
 def main(dataset_id, models):
--- src/utils/get_matrix.py
+++ src/utils/get_matrix.py
@@ -1,2 +1,35 @@
+import pandas as pd
+import numpy as np
+
+from src.utils.create_feature_and_featurename import create_featurenames
+
+
 def get_matrix_columns():
     return ['dataset - id', 'dataset - task type', 'dataset - number of classes', 'feature - name', 'operator', 'feature - type', 'feature - count', 'feature - mean', 'feature - std', 'feature - min', 'feature - max', 'feature - lower percentile', 'feature - 50 percentile', 'feature - upper percentile', 'feature - unique', 'feature - top', 'feature - freq', 'model', 'improvement']
+
+
+def add_new_featurenames(X_test):
+    # Get new dataset with feature names and metafeatures and replicate each feature (=each row) x times, that we can repeat thus row with similar values, but instead of the feature name, we add a new name consisting of all available operators and respective features
+    matrix_columns = get_matrix_columns()
+    featurenames = create_featurenames(X_test["feature - name"].values)
+    X_test_new = pd.DataFrame({
+        matrix_columns[0]: np.repeat((data := X_test[matrix_columns[0]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
+        matrix_columns[1]: np.repeat((data := X_test[matrix_columns[1]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
+        matrix_columns[2]: np.repeat((data := X_test[matrix_columns[2]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
+        matrix_columns[3]: np.repeat(np.array(featurenames), data.shape[0]).reshape(len(featurenames), data.shape[0]).T.flatten(),
+        matrix_columns[4]: np.repeat((data := X_test[matrix_columns[4]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
+        matrix_columns[5]: np.repeat((data := X_test[matrix_columns[5]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
+        matrix_columns[6]: np.repeat((data := X_test[matrix_columns[6]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
+        matrix_columns[7]: np.repeat((data := X_test[matrix_columns[7]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
+        matrix_columns[8]: np.repeat((data := X_test[matrix_columns[8]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
+        matrix_columns[9]: np.repeat((data := X_test[matrix_columns[9]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
+        matrix_columns[10]: np.repeat((data := X_test[matrix_columns[10]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
+        matrix_columns[11]: np.repeat((data := X_test[matrix_columns[11]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
+        matrix_columns[12]: np.repeat((data := X_test[matrix_columns[12]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
+        matrix_columns[13]: np.repeat((data := X_test[matrix_columns[13]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
+        matrix_columns[14]: np.repeat((data := X_test[matrix_columns[14]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
+        matrix_columns[15]: np.repeat((data := X_test[matrix_columns[15]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
+        matrix_columns[16]: np.repeat((data := X_test[matrix_columns[16]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
+        matrix_columns[17]: np.repeat((data := X_test[matrix_columns[17]].to_numpy()), len(featurenames)).reshape(-1, len(featurenames)).flatten(),
+    })
+    return X_test_new
