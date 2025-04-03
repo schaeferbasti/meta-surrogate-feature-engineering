2ce522e898ff97788ccece706cfdd996cac41b22
bastis007 <schaefer.bastian@mail.de>
2025-04-03 11:25:30 +0200

    Fix refactoring bugs, too (see Operator_Feature_Matrix.py) and reduce Best_Operations.parquet

--- src/SurrogateModel/SurrogateModel.py
+++ src/SurrogateModel/SurrogateModel.py
@@ -1,7 +1,5 @@
 import pandas as pd
-import numpy as np
 
-from src.utils.create_feature_and_featurename import create_featurenames
 from src.utils.get_data import get_openml_dataset_and_metadata
 from src.utils.get_matrix import get_matrix_columns
 from src.utils.get_metafeatures import get_numeric_pandas_metafeatures, get_categorical_pandas_metafeatures
@@ -17,9 +15,9 @@ def add_metadata(X_predict, dataset_metadata, models):
         for featurename in X_predict.columns:
             feature_df = pd.DataFrame(X_predict, columns=[featurename])
             feature_datatype = feature_df[featurename].dtype
-            if feature_datatype == np.number:
+            if pd.api.types.is_numeric_dtype(feature_df[featurename]):
                 feature_metadata_numeric = get_numeric_pandas_metafeatures(feature_df, featurename)
-                X_predict_new.loc[len(X_predict_new)] = [dataset_metadata["task_id"], dataset_metadata["task_type"], dataset_metadata["number_of_classes"], feature_metadata_numeric["feature - name"], str(feature_datatype), int(feature_metadata_numeric["feature - count"]), feature_metadata_numeric["feature - mean"], feature_metadata_numeric["feature - std"], feature_metadata_numeric["feature - min"], feature_metadata_numeric["feature - max"], feature_metadata_numeric["feature - lower percentile"], feature_metadata_numeric["feature - 50 percentile"], feature_metadata_numeric["feature - upper percentile"], None, None, None, model, None]
+                X_predict_new.loc[len(X_predict_new)] = [dataset_metadata["task_id"], dataset_metadata["task_type"], dataset_metadata["number_of_classes"], feature_metadata_numeric["feature - name"], "operator", str(feature_datatype), int(feature_metadata_numeric["feature - count"]), feature_metadata_numeric["feature - mean"], feature_metadata_numeric["feature - std"], feature_metadata_numeric["feature - min"], feature_metadata_numeric["feature - max"], feature_metadata_numeric["feature - lower percentile"], feature_metadata_numeric["feature - 50 percentile"], feature_metadata_numeric["feature - upper percentile"], None, None, None, model, None]
             else:
                 feature_metadata_categorical = get_categorical_pandas_metafeatures(feature_df, featurename)
                 X_predict_new.loc[len(X_predict_new)] = [dataset_metadata["task_id"], dataset_metadata["task_type"], dataset_metadata["number_of_classes"], feature_metadata_categorical["feature - name"], "operator", str(feature_datatype), int(feature_metadata_categorical["feature - count"]), None, None, None, None, None, None, None, feature_metadata_categorical["feature - unique"], feature_metadata_categorical["feature - top"], feature_metadata_categorical["feature - freq"], model, None]
@@ -46,7 +44,7 @@ def main(dataset_id, models):
     #  evaluation, prediction, best_operations = predict_operators_for_models(X, y, X_predict, y_predict, models=models, zeroshot=False)
     #  evaluation.to_parquet('Evaluation.parquet')
     prediction_result = pd.read_parquet("Prediction.parquet")
-    best_operations = prediction_result.nlargest(n=20, columns="predicted_improvement", keep="all")
+    best_operations = prediction_result.nlargest(n=20, columns="predicted_improvement", keep="first")
     best_operations.to_parquet("Best_Operations.parquet")
 
     # Multi-predictor (features + operator & improvement)
@@ -55,7 +53,7 @@ def main(dataset_id, models):
     #  multi_evaluation, multi_prediction = multi_predict_operators_for_models(X, y, X_predict, y_predict, models=models, zeroshot=False)
     #  multi_evaluation.to_parquet('Multi_Evaluation.parquet')
     multi_prediction_result = pd.read_parquet("Multi_Prediction.parquet")
-    multi_best_operations = multi_prediction_result.nlargest(n=20, columns="predicted_improvement", keep="all")
+    multi_best_operations = multi_prediction_result.nlargest(n=20, columns="predicted_improvement", keep="first")
     multi_best_operations.to_parquet("Multi_Best_Operations.parquet")
 
 
