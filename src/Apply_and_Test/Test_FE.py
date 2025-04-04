54b64056da3c11ab15208945d3df56d20f37c1fd
bastis007 <schaefer.bastian@mail.de>
2025-04-04 11:44:26 +0200

    Refactoring run_models file

--- src/Apply_and_Test/Test_FE.py
+++ src/Apply_and_Test/Test_FE.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from src.utils.get_data import get_openfe_data
 from src.utils.get_data import split_data, get_openml_dataset_split
-from src.utils.run_models import get_result
+from src.utils.run_models import get_model_score
 
 
 def main():
@@ -17,16 +17,16 @@ def main():
         dataset_id_and_model = test_file.split('FE_Data_')[-1].split('.')[0]
         dataset_id = dataset_id_and_model.split('_')[0]
         X_train, y_train, X_test, y_test = get_openml_dataset_split(int(dataset_id))
-        original_results = get_result(X_train, y_train, dataset_id)
+        original_results = get_model_score(X_train, y_train, X_test, y_test, dataset_id)
         result = pd.concat([result, original_results], ignore_index=True)
         print("Original Results: " + str(original_results))
         X_train, y_train, X_test, y_test = get_openfe_data(X_train, y_train, X_test, y_test)
-        openfe_results = get_result(X_train, y_train, dataset_id)
+        openfe_results = get_model_score(X_train, y_train, X_test, y_test, dataset_id)
         result = pd.concat([result, openfe_results], ignore_index=True)
         print("OpenFE Results: " + str(openfe_results))
         data = pd.read_parquet(test_file)
         X_train, y_train, X_test, y_test = split_data(data, target_label)
-        my_fe_results = get_result(X_train, y_train, dataset_id)
+        my_fe_results = get_model_score(X_train, y_train, X_test, y_test, dataset_id)
         result = pd.concat([result, my_fe_results], ignore_index=True)
         print("My Results: " + str(my_fe_results))
         result.to_parquet("Result_" + test_file)
--- src/Metadata/Operator_Model_Feature_Matrix_parallel.py
+++ src/Metadata/Operator_Model_Feature_Matrix_parallel.py
@@ -7,7 +7,7 @@ from src.utils.create_feature_and_featurename import create_feature_and_featuren
 from src.utils.get_data import get_openml_dataset_split_and_metadata
 from src.utils.get_matrix import get_matrix_columns
 from src.utils.get_operators import get_operators
-from src.utils.run_models import get_original_result
+from src.utils.run_models import get_model_score
 
 
 def main(dataset):
@@ -15,20 +15,25 @@ def main(dataset):
     result_matrix = pd.DataFrame(columns=columns)
     unary_operators, binary_operators = get_operators()
     X_train, y_train, X_test, y_test, dataset_metadata = get_openml_dataset_split_and_metadata(dataset)
-    original_results = get_original_result(X_train, y_train, dataset)
+    original_results = get_model_score(X_train, y_train, X_test, y_test, dataset)
     for feature1 in X_train.columns:
         for feature2 in X_train.columns:
             for operator in binary_operators:
-                feature, featurename = create_feature_and_featurename(feature1=X_train[feature1],
+                train_feature, featurename = create_feature_and_featurename(feature1=X_train[feature1],
                                                                       feature2=X_train[feature2], operator=operator)
-                new_rows = get_result(X_train, y_train, dataset_metadata, feature, featurename, original_results)
+                test_feature, featurename = create_feature_and_featurename(feature1=X_test[feature1],
+                                                                            feature2=X_test[feature2],
+                                                                            operator=operator)
+                new_rows = get_result(X_train, y_train, X_test, y_test, dataset_metadata, train_feature, test_feature, featurename, original_results)
                 result_matrix = pd.concat([result_matrix, pd.DataFrame(new_rows)], ignore_index=True)
                 result_matrix.to_parquet("Operator_Model_Feature_Matrix_2_" + str(dataset) + ".parquet")
     for feature1 in X_train.columns:
         for operator in unary_operators:
-            feature, featurename = create_feature_and_featurename(feature1=X_train[feature1], feature2=None,
+            train_feature, featurename = create_feature_and_featurename(feature1=X_train[feature1], feature2=None,
                                                                   operator=operator)
-            new_rows = get_result(X_train, y_train, dataset_metadata, feature, featurename, original_results)
+            test_feature, featurename = create_feature_and_featurename(feature1=X_test[feature1], feature2=None,
+                                                                       operator=operator)
+            new_rows = get_result(X_train, y_train, X_test, y_test, dataset_metadata, train_feature, test_feature, featurename, original_results)
             result_matrix = pd.concat([result_matrix, pd.DataFrame(new_rows)], ignore_index=True)
             result_matrix.to_parquet("Operator_Model_Feature_Matrix_2_" + str(dataset) + ".parquet")
     result_matrix.to_parquet("Operator_Model_Feature_Matrix_2_" + str(dataset) + ".parquet")
