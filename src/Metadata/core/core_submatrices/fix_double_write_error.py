8fefa1aafa70d5146d7274e610152356e480d2b4
bastis007 <schaefer.bastian@mail.de>
2025-06-17 20:10:20 +0200

    Minor changes

--- src/Metadata/core/core_submatrices/fix_double_write_error.py
+++ src/Metadata/core/core_submatrices/fix_double_write_error.py
@@ -10,9 +10,11 @@ def main():
             core_files.append(file)
 
     for core_file in core_files:
+        print(core_file)
         result_matrix = pd.read_parquet(core_file)
         result_matrix.drop_duplicates(inplace=True)
         result_matrix.to_parquet(core_file)
+
 
 if __name__ == "__main__":
     main()
