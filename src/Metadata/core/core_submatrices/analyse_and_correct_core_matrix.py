9318a09ce0a909004fcee2a434fe2c2a0590b859
bastis007 <schaefer.bastian@mail.de>
2025-06-17 20:46:08 +0200

    Add analysis of complete and incomplete core files

---
+++ src/Metadata/core/core_submatrices/analyse_and_correct_core_matrix.py
@@ -0,0 +1,56 @@
+import os
+import pandas as pd
+
+from src.utils.get_operators import get_operators
+
+
+def remove_duplicate_columns():
+    files = os.listdir()
+    core_files = []
+    for file in files:
+        if file.endswith('.parquet'):
+            core_files.append(file)
+
+    for core_file in core_files:
+        print(core_file)
+        result_matrix = pd.read_parquet(core_file)
+        result_matrix.drop_duplicates(inplace=True)
+        result_matrix.to_parquet(core_file)
+
+
+def list_incomplete_df():
+    files = os.listdir()
+    core_files = []
+    for file in files:
+        if file.endswith('.parquet'):
+            core_files.append(file)
+    core_files.sort()
+
+    list_of_incomplete_df = []
+    for core_file in core_files:
+        print(core_file)
+        result_matrix = pd.read_parquet(core_file)
+        dataset_id = core_file.split('.')[0].split('Core')[-1]
+        unary_operators, binary_operators = get_operators()
+        list_of_operators = unary_operators + binary_operators + ["delete", "add", "subtract", "multiply", "divide"]
+        list_of_operators.remove("+")
+        list_of_operators.remove("-")
+        list_of_operators.remove("*")
+        list_of_operators.remove("/")
+        if set(list_of_operators).issubset(set(result_matrix['operator'])):
+            print("✅ All required operators are present.")
+        else:
+            print("❌ Some required operators are missing.")
+            list_of_incomplete_df.append(dataset_id)
+    print("Number of Saved Files: " + str(len(core_files)))
+    print("Number of Incomplete Files: " + str(len(list_of_incomplete_df)))
+    print("Number of Complete Files: " + str(len(core_files) - len(list_of_incomplete_df)))
+    print("List of Incomplete Files: " + str(list_of_incomplete_df))
+
+def main():
+    # remove_duplicate_columns()
+    list_incomplete_df()
+
+
+if __name__ == "__main__":
+    main()
