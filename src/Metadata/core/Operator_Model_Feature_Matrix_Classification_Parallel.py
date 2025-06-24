b3e48bcdead5ea03f14c6c4891bf047ccd5eb740
bastis007 <schaefer.bastian@mail.de>
2025-06-24 12:43:16 +0200

    Make rerunning core matrix faster by checking what is already there

--- src/Metadata/core/Operator_Model_Feature_Matrix_Classification_Parallel.py
+++ src/Metadata/core/Operator_Model_Feature_Matrix_Classification_Parallel.py
@@ -4,10 +4,38 @@ import numpy as np
 import pandas as pd
 
 from src.utils.create_feature_and_featurename import create_feature_and_featurename, extract_operation_and_original_features
-from src.utils.get_data import get_openml_dataset_split_and_metadata
+from src.utils.get_data import get_openml_dataset_split_and_metadata, get_openml_dataset
 from src.utils.get_matrix import get_matrix_core_columns
 from src.utils.get_operators import get_operators
 from src.utils.run_models import get_model_score, run_autogluon_lgbm_classification
+
+
+def check_if_complete(result_matrix):
+    check = False
+    dataset_id = result_matrix["dataset - id"].values[0]
+    X, y = get_openml_dataset(int(dataset_id))
+    n_features = len(X.columns)
+    n_lines_calculated = n_features * n_features * 13 + n_features * 10 + n_features
+    n_lines_present = result_matrix.shape[0]
+    if n_lines_present == n_lines_calculated:
+        check = True
+    return check
+
+
+def check_if_operator_is_there(result_matrix, operator):
+    check = False
+    dataset_id = result_matrix["dataset - id"].values[0]
+    X, y = get_openml_dataset(int(dataset_id))
+    n_features = len(X.columns)
+    n_lines_calculated = n_features
+    operator_count = result_matrix['operator'].value_counts()
+    try:
+        n_lines_present = operator_count[operator]
+        if n_lines_present == n_lines_calculated:
+            check = True
+    except KeyError:
+        return False
+    return check
 
 
 def get_core_result_feature_generation_classification(X_train, y_train, X_test, y_test, dataset_metadata, train_feature, test_feature, featurename, original_results):
@@ -69,11 +97,72 @@ def calc_relative_improvement(original_score, modified_score):
         return (original_score - modified_score) / modified_score
 
 
+def continue_calculating_improvement_classification(result_matrix, dataset):
+    unary_operators, binary_operators = get_operators()
+    X_train, y_train, X_test, y_test, dataset_metadata = get_openml_dataset_split_and_metadata(dataset)
+    X_train_copy = X_train.copy()
+    X_test_copy = X_test.copy()
+    original_results = get_model_score(X_train, y_train, X_test, y_test, dataset)
+    if check_if_operator_is_there(result_matrix, "delete"):
+        print("Result matrix contains all delete operations")
+    else:
+        for feature1 in X_train_copy.columns:
+            X_train_reduced = X_train_copy.drop(feature1, axis=1)
+            X_test_reduced = X_test_copy.drop(feature1, axis=1)
+            featurename = "without - " + str(feature1)
+            new_rows = get_core_result_feature_selection_classification(X_train_reduced, y_train, X_test_reduced, y_test,
+                                                                        dataset_metadata, featurename, original_results)
+            result_matrix = pd.concat([result_matrix, pd.DataFrame(new_rows)], ignore_index=True)
+            result_matrix.to_parquet("Operator_Model_Feature_Matrix_Core" + str(dataset) + ".parquet")
+    missing_binary_operators = []
+    for binary_operator in binary_operators:
+        if binary_operator == "+":
+            binary_operator = "add"
+        if binary_operator == "-":
+            binary_operator = "subtract"
+        if binary_operator == "*":
+            binary_operator = "multiply"
+        if binary_operator == "/":
+            binary_operator = "/"
+        if check_if_operator_is_there(result_matrix, binary_operator):
+            print("Result matrix contains all delete operations")
+        else:
+            missing_binary_operators.append(binary_operator)
+    for feature1 in X_train_copy.columns:
+        for feature2 in X_train_copy.columns:
+            for operator in missing_binary_operators:
+                train_feature, featurename = create_feature_and_featurename(feature1=X_train[feature1], feature2=X_train[feature2], operator=operator)
+                test_feature, featurename = create_feature_and_featurename(feature1=X_test[feature1], feature2=X_test[feature2], operator=operator)
+                new_rows = get_core_result_feature_generation_classification(X_train, y_train, X_test, y_test, dataset_metadata, train_feature, test_feature, featurename, original_results)
+                result_matrix = pd.concat([result_matrix, pd.DataFrame(new_rows)], ignore_index=True)
+                result_matrix.to_parquet("Operator_Model_Feature_Matrix_Core" + str(dataset) + ".parquet")
+    missing_unary_operators = []
+    for unary_operator in unary_operators:
+        if check_if_operator_is_there(result_matrix, unary_operator):
+            print("Result matrix contains all delete operations")
+        else:
+            missing_unary_operators.append(unary_operator)
+    for feature1 in X_train_copy.columns:
+        for operator in missing_unary_operators:
+            train_feature, featurename = create_feature_and_featurename(feature1=X_train[feature1], feature2=None, operator=operator)
+            test_feature, featurename = create_feature_and_featurename(feature1=X_test[feature1], feature2=None, operator=operator)
+            new_rows = get_core_result_feature_generation_classification(X_train, y_train, X_test, y_test, dataset_metadata, train_feature, test_feature, featurename, original_results)
+            result_matrix = pd.concat([result_matrix, pd.DataFrame(new_rows)], ignore_index=True)
+            result_matrix.to_parquet("Operator_Model_Feature_Matrix_Core" + str(dataset) + ".parquet")
+        result_matrix.to_parquet("Operator_Model_Feature_Matrix_Core" + str(dataset) + ".parquet")
+    result_matrix.to_parquet("Operator_Model_Feature_Matrix_Core" + str(dataset) + ".parquet")
+
+
 def main(dataset):
     print("Classification Dataset: " + str(dataset))
     try:
-        result_matrix = pd.read_parquet("Operator_Model_Feature_Matrix_Core" + str(dataset) + ".parquet")
-        print(result_matrix)
+        result_matrix = pd.read_parquet("core_submatrices/Operator_Model_Feature_Matrix_Core" + str(dataset) + ".parquet")
+        check = check_if_complete(result_matrix)
+        if check:
+            print("Dataset " + str(dataset) + " is complete")
+        else:
+            print("Dataset " + str(dataset) + " is NOT complete")
+            continue_calculating_improvement_classification(result_matrix, dataset)
     except FileNotFoundError:
         columns = get_matrix_core_columns()
         result_matrix = pd.DataFrame(columns=columns)
@@ -112,4 +201,4 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run Metadata Classification')
     parser.add_argument('--dataset_classification', type=int, required=True, help='AMLB Classification Dataset')
     args = parser.parse_args()
-    main(args.dataset_classification)
+    main(args.dataset)
