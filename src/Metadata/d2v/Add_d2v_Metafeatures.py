adc574ea9b8fe238f47e13bdee43a6604706f77c
bastis007 <schaefer.bastian@mail.de>
2025-06-30 16:47:47 +0200

    d2v for cluster and multiple datasets

--- src/Metadata/d2v/Add_d2v_Metafeatures.py
+++ src/Metadata/d2v/Add_d2v_Metafeatures.py
@@ -112,13 +112,17 @@ def add_d2v_metadata_columns(dataset_metadata, X_train, result_matrix):
 
 
 def main():
-    result_matrix = pd.read_parquet("../core/Core_Matrix_Example.parquet")
+    result_matrix = pd.read_parquet("src/Metadata/core/Core_Matrix_Example.parquet")
+    columns = get_additional_d2v_columns()
+    result_matrix_pandas = pd.DataFrame(columns=columns)
     for dataset, _ in result_matrix.groupby('dataset - id'):
         print("Dataset: " + str(dataset))
         X_train, y_train, X_test, y_test, dataset_metadata = get_openml_dataset_split_and_metadata(dataset)
-        result_matrix = add_d2v_metadata_columns(dataset_metadata, X_train, result_matrix)
-        result_matrix.to_parquet("d2v_metafeatures_" + str(dataset) + ".parquet")
-    result_matrix.to_parquet("d2v_metafeatures.parquet")
+        result_matrix_dataset = result_matrix[result_matrix['dataset - id'] == dataset]
+        result_matrix_dataset = add_d2v_metadata_columns(dataset_metadata, X_train, result_matrix_dataset)
+        result_matrix_pandas = pd.concat([result_matrix_pandas, result_matrix_dataset], axis=1)
+        result_matrix.to_parquet("src/Metadata/d2v/D2V_Matrix_Complete" + str(dataset) + ".parquet")
+    result_matrix.to_parquet("src/Metadata/d2v/D2V_Matrix_Complete.parquet")
 
 
 if __name__ == '__main__':
