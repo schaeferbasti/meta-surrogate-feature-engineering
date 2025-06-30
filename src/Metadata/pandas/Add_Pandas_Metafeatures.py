61bc7c40c054828b8ba5b1031fcf9e0b27152612
bastis007 <schaefer.bastian@mail.de>
2025-06-30 11:43:56 +0200

    Update Pandas Metafeaturegeneration for Core Matrix with more than one dataset

--- src/Metadata/pandas/Add_Pandas_Metafeatures.py
+++ src/Metadata/pandas/Add_Pandas_Metafeatures.py
@@ -78,13 +78,17 @@ def add_pandas_metadata_columns(dataset_metadata, X_train, result_matrix):
 
 
 def main():
-    result_matrix = pd.read_parquet("../core/Core_Matrix_Example.parquet")
+    result_matrix = pd.read_parquet("../core/Core_Matrix_Complete.parquet")
+    columns = get_additional_pandas_columns()
+    result_matrix_pandas = pd.DataFrame(columns=columns)
     for dataset, _ in result_matrix.groupby('dataset - id'):
         print("Dataset: " + str(dataset))
         X_train, y_train, X_test, y_test, dataset_metadata = get_openml_dataset_split_and_metadata(dataset)
-        result_matrix = add_pandas_metadata_columns(dataset_metadata, X_train, result_matrix)
-        result_matrix.to_parquet("pandas_metafeatures_" + str(dataset) + ".parquet")
-    result_matrix.to_parquet("pandas_metafeatures.parquet")
+        result_matrix_dataset = result_matrix[result_matrix['dataset - id'] == dataset]
+        result_matrix_dataset = add_pandas_metadata_columns(dataset_metadata, X_train, result_matrix_dataset)
+        result_matrix_pandas = pd.concat([result_matrix_pandas, result_matrix_dataset], axis=1)
+        result_matrix.to_parquet("Matrix_Complete_Pandas" + str(dataset) + ".parquet")
+    result_matrix.to_parquet("Matrix_Complete_Pandas.parquet")
 
 
 if __name__ == '__main__':
