6abcc0a9b2cc89a88b970323588e13285c3f0ccd
bastis007 <schaefer.bastian@mail.de>
2025-06-30 17:51:36 +0200

    Correct Pandas Procedure

--- src/Metadata/pandas/Add_Pandas_Metafeatures.py
+++ src/Metadata/pandas/Add_Pandas_Metafeatures.py
@@ -78,17 +78,20 @@ def add_pandas_metadata_columns(dataset_metadata, X_train, result_matrix):
 
 
 def main():
-    result_matrix = pd.read_parquet("src/Metadata/core/Core_Matrix_Complete.parquet")
+    result_matrix = pd.read_parquet("../core/Core_Matrix_Complete.parquet")
     columns = get_additional_pandas_columns()
+    result_matrix_columns = result_matrix.columns.values.tolist()
+    columns = columns + result_matrix_columns
     result_matrix_pandas = pd.DataFrame(columns=columns)
     for dataset, _ in result_matrix.groupby('dataset - id'):
         print("Dataset: " + str(dataset))
         X_train, y_train, X_test, y_test, dataset_metadata = get_openml_dataset_split_and_metadata(int(str(dataset)))
         result_matrix_dataset = result_matrix[result_matrix['dataset - id'] == dataset]
         result_matrix_dataset = add_pandas_metadata_columns(dataset_metadata, X_train, result_matrix_dataset)
-        result_matrix_pandas = pd.concat([result_matrix_pandas, result_matrix_dataset], axis=1)
-        result_matrix_pandas.to_parquet("src/Metadata/pandas/Pandas_Matrix_Complete" + str(dataset) + ".parquet")
-    result_matrix_pandas.to_parquet("src/Metadata/pandas/Pandas_Matrix_Complete.parquet")
+        result_matrix_pandas.columns = result_matrix_dataset.columns
+        result_matrix_pandas = pd.concat([result_matrix_pandas, result_matrix_dataset], axis=0)
+        result_matrix_pandas.to_parquet("Pandas_Matrix_Complete" + str(dataset) + ".parquet")
+    result_matrix_pandas.to_parquet("Pandas_Matrix_Complete.parquet")
 
 
 if __name__ == '__main__':
