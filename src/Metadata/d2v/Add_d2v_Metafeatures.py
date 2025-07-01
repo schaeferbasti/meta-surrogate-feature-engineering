57d009fe79781c378bc15949c4e966d5e132cd15
bastis007 <schaefer.bastian@mail.de>
2025-07-01 10:35:45 +0200

    Skip if there is an error with feature

--- src/Metadata/d2v/Add_d2v_Metafeatures.py
+++ src/Metadata/d2v/Add_d2v_Metafeatures.py
@@ -126,16 +126,19 @@ def main():
         try:
             pd.read_parquet("src/Metadata/d2v/D2V_Matrix_Complete" + str(dataset) + ".parquet")
         except FileNotFoundError:
-            counter += 1
-            start_dataset = time.time()
-            X_train, y_train, X_test, y_test, dataset_metadata = get_openml_dataset_split_and_metadata(int(str(dataset)))
-            result_matrix_dataset = result_matrix[result_matrix['dataset - id'] == dataset]
-            result_matrix_dataset = add_d2v_metadata_columns(dataset_metadata, X_train, result_matrix_dataset)
-            result_matrix_pandas.columns = result_matrix_dataset.columns
-            result_matrix_pandas = pd.concat([result_matrix_pandas, result_matrix_dataset], axis=0)
-            result_matrix_pandas.to_parquet("src/Metadata/d2v/D2V_Matrix_Complete" + str(dataset) + ".parquet")
-            end_dataset = time.time()
-            print("Time for d2v on Dataset " + str(dataset) + ": " + str(end_dataset - start_dataset))
+            try:
+                counter += 1
+                start_dataset = time.time()
+                X_train, y_train, X_test, y_test, dataset_metadata = get_openml_dataset_split_and_metadata(int(str(dataset)))
+                result_matrix_dataset = result_matrix[result_matrix['dataset - id'] == dataset]
+                result_matrix_dataset = add_d2v_metadata_columns(dataset_metadata, X_train, result_matrix_dataset)
+                result_matrix_pandas.columns = result_matrix_dataset.columns
+                result_matrix_pandas = pd.concat([result_matrix_pandas, result_matrix_dataset], axis=0)
+                result_matrix_pandas.to_parquet("src/Metadata/d2v/D2V_Matrix_Complete" + str(dataset) + ".parquet")
+                end_dataset = time.time()
+                print("Time for d2v on Dataset " + str(dataset) + ": " + str(end_dataset - start_dataset))
+            except TypeError:
+                continue
     result_matrix_pandas.to_parquet("src/Metadata/d2v/D2V_Matrix_Complete.parquet")
     end = time.time()
     print("Time for complete d2v MF: " + str(end - start) + " on " + str(counter) + " datasets.")
