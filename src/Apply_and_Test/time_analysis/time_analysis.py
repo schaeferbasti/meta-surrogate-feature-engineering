fdd2321176e477e900f0d28b7ad4b813b3aca014
bastis007 <schaefer.bastian@mail.de>
2025-07-04 16:56:04 +0200

    Add time analysis of MFEs

---
+++ src/Apply_and_Test/time_analysis/time_analysis.py
@@ -0,0 +1,70 @@
+import os
+
+import openml
+import pandas as pd
+from matplotlib import pyplot as plt
+
+from src.Apply_and_Test.analyse_results import insert_line_breaks
+
+
+def main():
+    times = {}
+
+    log_files = os.listdir()
+
+    for log_file in log_files:
+        if log_file.endswith('.out'):
+            f = open(log_file, "r")
+            lines = f.readlines()
+            method = log_file.split("_")[0]
+
+            for line in lines:
+                if line.__contains__("on") and line.__contains__("datasets"):
+                    continue
+                elif line.startswith("Time for"):
+                    time = float(line.split(":")[-1])
+                    dataset = line.split(":")[0].split("Dataset ")[-1]
+                    method_and_dataset = str(method) + " - " + str(dataset)
+                    times.update({method_and_dataset: time})
+    print(times)
+
+    df = pd.DataFrame(list(times.items()), columns=['method_dataset', 'value'])
+    df[['method', 'dataset']] = df['method_dataset'].str.split(' - ', expand=True)
+    df['dataset'] = df['dataset'].astype(int)
+
+    df_pivot = df.pivot(index="dataset", columns="method", values="value")
+    df_pivot = df_pivot.sort_index()  # Sort by dataset ID
+
+    datasets = df_pivot.index.astype(str)
+    dataset_list = []
+    for dataset in datasets.tolist():
+        task = openml.tasks.get_task(
+            int(dataset),
+            download_splits=True,
+            download_data=True,
+            download_qualities=True,
+            download_features_meta_data=True,
+        )
+        dataset = task.get_dataset().name
+        dataset_list.append(dataset)
+    dataset_list_wrapped = [insert_line_breaks(name, max_len=15) for name in dataset_list]
+
+    # Plot
+    plt.figure(figsize=(12, 6))
+    for method in df_pivot.columns:
+        plt.plot(dataset_list_wrapped, df_pivot[method], marker='o', label=method)
+
+    plt.xlabel("Dataset ID")
+    plt.xticks(rotation=45)  # or 90
+    plt.yscale('log')  # Optional: only if values vary a lot
+    plt.ylabel("Time in seconds")
+    plt.title("Time for generating metafeatures by FE Method per Dataset")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig("Time_for_FE.png")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
