ce19b88e4aadb68072589a91aa55fb3e9ee58b1e
bastis007 <schaefer.bastian@mail.de>
2025-07-06 15:53:33 +0200

    Add method for adding group column

--- src/Metadata/mfe/Add_MFE_Metafeatures.py
+++ src/Metadata/mfe/Add_MFE_Metafeatures.py
@@ -5,8 +5,87 @@ import pandas as pd
 
 from src.utils.create_feature_and_featurename import create_feature
 from src.utils.get_data import get_openml_dataset_split_and_metadata
-from src.utils.get_matrix import get_additional_mfe_columns
+from src.utils.get_matrix import get_additional_mfe_columns, get_additional_mfe_columns_group
 from src.utils.get_metafeatures import get_mfe_dataset_metadata
+
+
+def add_mfe_metadata_columns_group(X_train, y_train, result_matrix, group):
+    columns = get_additional_mfe_columns_group(group)
+    new_columns = pd.DataFrame(index=result_matrix.index, columns=columns)
+    for row in result_matrix.iterrows():
+        featurename = row[1][1]
+        X_train_copy = X_train.copy()
+        if featurename.startswith("without"):
+            feature_to_delete = featurename.split(" - ")[1]
+            X_train_copy = X_train_copy.drop(feature_to_delete, axis=1)
+        else:
+            features = featurename.split("(")[1].replace(")", "").replace(" ", "")
+            if "," in features:
+                featurename1 = features.split(",")[0]
+                feature1 = X_train_copy[featurename1]
+                featurename2 = features.split(",")[1]
+                feature2 = X_train_copy[featurename2]
+            else:
+                featurename1 = features
+                feature1 = X_train_copy[featurename1]
+                feature2 = None
+            new_feature = create_feature(feature1, feature2, featurename)
+            new_feature_df = pd.DataFrame(new_feature, columns=[featurename])
+            new_feature_df.index = X_train_copy.index
+            X_train_copy = pd.concat([X_train_copy, new_feature_df], axis=1)
+        X = X_train_copy.replace([np.inf, -np.inf], np.nan).fillna(0).to_numpy()
+        y = y_train.to_numpy()
+        dataset_metadata_mfe, dataset_metadata_names, dataset_metadata_groups = get_mfe_dataset_metadata(X, y, group)
+        metafeatures = dataset_metadata_mfe[1]
+        new_row = pd.DataFrame([metafeatures], columns=dataset_metadata_names)
+        matching_indices = result_matrix[result_matrix["feature - name"] == str(featurename)].index
+        for idx in matching_indices:
+            new_columns.loc[idx] = new_row.iloc[0]
+    insert_position = result_matrix.shape[1] - 2
+    result_matrix = pd.concat([result_matrix.iloc[:, :insert_position], new_columns, result_matrix.iloc[:, insert_position:]], axis=1)
+    return result_matrix
+
+
+def add_mfe_metadata_columns_groups(X_train, y_train, result_matrix, groups):
+    columns_list = []
+    for group in groups:
+        columns_list.extend(group)
+    new_columns = pd.DataFrame(index=result_matrix.index, columns=columns_list)
+    for row in result_matrix.iterrows():
+        featurename = row[1][1]
+        X_train_copy = X_train.copy()
+        if featurename.startswith("without"):
+            feature_to_delete = featurename.split(" - ")[1]
+            X_train_copy = X_train_copy.drop(feature_to_delete, axis=1)
+        else:
+            features = featurename.split("(")[1].replace(")", "").replace(" ", "")
+            if "," in features:
+                featurename1 = features.split(",")[0]
+                feature1 = X_train_copy[featurename1]
+                featurename2 = features.split(",")[1]
+                feature2 = X_train_copy[featurename2]
+            else:
+                featurename1 = features
+                feature1 = X_train_copy[featurename1]
+                feature2 = None
+            new_feature = create_feature(feature1, feature2, featurename)
+            new_feature_df = pd.DataFrame(new_feature, columns=[featurename])
+            new_feature_df.index = X_train_copy.index
+            X_train_copy = pd.concat([X_train_copy, new_feature_df], axis=1)
+        X = X_train_copy.replace([np.inf, -np.inf], np.nan).fillna(0).to_numpy()
+        y = y_train.to_numpy()
+        metafeatures = []
+        for group in groups:
+            dataset_metafeatures_group_mfe, dataset_metadata_group_names, _ = get_mfe_dataset_metadata(X, y, group)
+            metafeatures.extend(dataset_metafeatures_group_mfe[1])
+        new_columns.columns = columns_list
+        new_row = pd.DataFrame([metafeatures], columns=columns_list)
+        matching_indices = result_matrix[result_matrix["feature - name"] == str(featurename)].index
+        for idx in matching_indices:
+            new_columns.loc[idx] = new_row.iloc[0]
+    insert_position = result_matrix.shape[1] - 2
+    result_matrix = pd.concat([result_matrix.iloc[:, :insert_position], new_columns, result_matrix.iloc[:, insert_position:]], axis=1)
+    return result_matrix
 
 
 def add_mfe_metadata_columns(X_train, y_train, result_matrix):
