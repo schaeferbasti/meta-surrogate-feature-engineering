9110b05d85140a154e43807250876733fd90fb3c
bastis007 <schaefer.bastian@mail.de>
2025-07-07 11:04:00 +0200

    Make time analysis comparable by only using d2v data

--- src/Result_Analysis/time_analysis_sm/time_analysis_sm.py
+++ src/Result_Analysis/time_analysis_sm/time_analysis_sm.py
@@ -37,55 +37,46 @@ def main():
                 elif line.startswith(comp_sentence):
                     time = float(line.split(comp_sentence)[1])
                     comp_times.append(time)
-                elif line.startswith(pred_sentence):
-                    pred_time = float(line.split(pred_sentence)[1])
+                # elif line.startswith(pred_sentence):
+                    # pred_time = float(line.split(pred_sentence)[1])
 
-            # Handle timeout case
-            if timeout:
-                new_row = pd.DataFrame([{
-                    "Method": method,
-                    "Dataset": dataset,
-                    "Task": "Calculate Comparison Matrix",
-                    "Time": 3600.0
-                }])
-                times = pd.concat([times, new_row], ignore_index=True)
-
-            else:
-                # Use last occurrence for Comparison Matrix
-                if comp_times:
+                # Handle timeout case
+                if timeout:
                     new_row = pd.DataFrame([{
                         "Method": method,
                         "Dataset": dataset,
                         "Task": "Calculate Comparison Matrix",
-                        "Time": comp_times[-1]
+                        "Time": 3600.0
                     }])
                     times = pd.concat([times, new_row], ignore_index=True)
 
-                # Add Predict Improvement time
-                if pred_time is not None:
-                    new_row = pd.DataFrame([{
-                        "Method": method,
-                        "Dataset": dataset,
-                        "Task": "Predict Improvement",
-                        "Time": pred_time
-                    }])
-                    times = pd.concat([times, new_row], ignore_index=True)
+                else:
+                    # Use last occurrence for Comparison Matrix
+                    if comp_times:
+                        new_row = pd.DataFrame([{
+                            "Method": method,
+                            "Dataset": dataset,
+                            "Task": "Calculate Comparison Matrix",
+                            "Time": comp_times[-1]
+                        }])
+                        times = pd.concat([times, new_row], ignore_index=True)
 
     time_per_method = times.groupby("Method")["Time"].sum().sort_values(ascending=False)
-    average_time_per_method = times.groupby("Method")["Time"].mean().sort_values(ascending=False)
-
+    average_recursion = time_per_method.values[0] / 12
+    average_best = time_per_method.values[1] / 12
+    average_time_per_method = pd.Series([average_recursion, average_best], index=["Recursion", "Best"])
     # Plot
     plt.figure(figsize=(10, 6))
-    time_per_method.plot(kind='bar', color='skyblue', label='Total Time per Method')
-    average_time_per_method.plot(kind='bar', width=0.3, color='orange', label='Average Time per Method')
+    time_per_method.plot(kind='bar', color='skyblue', label='Total Time for 12 Datasets per Surrogate Model')
+    average_time_per_method.plot(kind='bar', width=0.3, color='orange', label='Average Time over 12 Datasets per Surrogate Model')
     plt.legend()
     plt.xlabel("Method")
     plt.ylabel("Time in seconds")
-    plt.title("Time per Method")
+    plt.title("Time Usage of Surrogate Models")
     plt.xticks(rotation=45, ha="right")
     # plt.yscale('log')
     plt.tight_layout()
-    plt.savefig("Time_for_SM_per_Method.png")
+    plt.savefig("Time_SM_oneshot_recursive.png")
     plt.show()
 
 
