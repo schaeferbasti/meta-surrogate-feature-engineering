8ba352d069b9a90f9003db31406c98368898bf4e
bastis007 <schaefer.bastian@mail.de>
2025-07-10 12:16:02 +0200

    Refactor analyse results

--- src/Apply_and_Test/analyse_results.py
+++ src/Apply_and_Test/analyse_results.py
@@ -14,6 +14,91 @@ def insert_line_breaks(name, max_len=20):
         return name
 
 
+def plot_autogluon_score_graph(dataset_list_wrapped, df_pivot_val, name):
+    plt.figure(figsize=(12, 6))
+    for method in df_pivot_val.columns:
+        plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
+    plt.xlabel("Dataset ID")
+    plt.xticks(rotation=45)  # or 90
+    plt.ylabel("Autogluon Val Score (flipped)")
+    # plt.yscale("log") # '', '', 'function', 'functionlog
+    plt.title("Autogluon Val Score by FE Method per Dataset")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig("test_results/Autogluon_" + name + "_Score_by_FE_Method.png")
+    plt.show()
+
+
+def plot_random_vs_me_graph(dataset_list_wrapped, df_pivot_val, name):
+    plt.figure(figsize=(12, 6))
+    for method in df_pivot_val.columns:
+        if method == "Best Random":
+            plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
+        elif method.startswith("pandas"):
+            plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
+        elif method.startswith("d2v"):
+            plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
+        elif method.startswith("MFE"):
+            plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
+        elif method.startswith("Original"):
+            plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
+    plt.xlabel("Dataset ID")
+    plt.xticks(rotation=45)  # or 90
+    plt.ylabel("Autogluon " + name + " Score (flipped)")
+    plt.title("Autogluon " + name + " Score by FE Method per Dataset (Best random vs. our method)")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig("test_results/Random_vs_Me_" + name + "_graph.png")
+    plt.show()
+
+
+def plot_count_best(df_pivot_val, df_pivot_test, name):
+    minValueIndex_val = df_pivot_val.idxmin(axis=1).value_counts()
+    minValueIndex_test = df_pivot_test.idxmin(axis=1).value_counts()
+    # Plot
+    plt.figure(figsize=(10, 6))
+    minValueIndex_val.plot(kind='bar', color='skyblue', label='Count of best val result on number of datasets')
+    minValueIndex_test.plot(kind='bar', width=0.3, color='darkblue',
+                            label='Count of best test result on number of datasets')
+    plt.legend()
+    plt.xlabel("Method")
+    plt.ylabel("Best result on number of datasets")
+    plt.title("Which method is the best?")
+    plt.xticks(rotation=45, ha="right")
+    plt.tight_layout()
+    plt.savefig("test_results/Count_Best_" + name + "bar.png")
+    plt.show()
+
+
+def plot_avg_perc_impr(baseline_col, df_pivot, name):
+    improvement_test = pd.DataFrame()
+    for method in df_pivot.columns:
+        if method == baseline_col:
+            continue
+        improvement = (df_pivot[baseline_col] - df_pivot[method]) / df_pivot[baseline_col] * 100
+        improvement_test[method] = improvement
+    avg_improvement_test = improvement_test.mean().sort_values(ascending=False)
+    # for i, val in enumerate(avg_improvement_test):
+    #    plt.text(i, val + (1 if val >= 0 else -1), f"{val:.2f}%", ha='center', va='bottom' if val >= 0 else 'top')
+    plt.figure(figsize=(10, 6))
+    # avg_improvement_test.plot(kind="bar", color="skyblue")
+    bars = avg_improvement_test.plot(kind="bar", color="skyblue")
+    for i, val in enumerate(avg_improvement_test):
+        y = -1 if val >= 0 else 0  # adjust offset for spacing
+        plt.text(i, y, f"{val:.2f}%", ha='center', va='top' if val >= 0 else 'bottom', color='black')
+    plt.axhline(0, color="black", linewidth=0.8)
+    plt.title("Average Percentage Improvement " + name + " over original Dataset")
+    plt.xlabel("Method")
+    plt.ylabel("Average Improvement (%)")
+    plt.xticks(rotation=45, ha="right")
+    plt.grid(True, linestyle="--", alpha=0.6)
+    plt.tight_layout()
+    plt.savefig("test_results/Average_Percentage_Improvement_" + name + ".png")
+    plt.show()
+
+
 def analyse_results():
     result_files = glob.glob("test_results/Result_*.parquet")
     all_results = []
@@ -50,227 +135,28 @@ def analyse_results():
         dataset_list.append(dataset)
     dataset_list_wrapped = [insert_line_breaks(name, max_len=15) for name in dataset_list]
 
-    # Plot
-    plt.figure(figsize=(12, 6))
-    for method in df_pivot_val.columns:
-        plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
-    plt.xlabel("Dataset ID")
-    plt.xticks(rotation=45)  # or 90
-    plt.ylabel("Autogluon Val Score (flipped)")
-    # plt.yscale("log") # '', '', 'function', 'functionlog
-    plt.title("Autogluon Val Score by FE Method per Dataset")
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-    plt.savefig("test_results/Autogluon_Val_Score_by_FE_Method.png")
-    plt.show()
-
-    # Plot
-    plt.figure(figsize=(12, 6))
-    for method in df_pivot_val.columns:
-        plt.plot(dataset_list_wrapped, df_pivot_test[method], marker='o', label=method)
-    plt.xlabel("Dataset ID")
-    plt.xticks(rotation=45)  # or 90
-    plt.ylabel("Autogluon Test Score (flipped)")
-    # plt.yscale("log")  # '', '', 'function', 'functionlog
-    plt.title("Autogluon Test Score by FE Method per Dataset")
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-    plt.savefig("test_results/Autogluon_Test_Score_by_FE_Method.png")
-    plt.show()
-
-
-    # ----- Compute percentage improvement over baseline -----
     baseline_col = "Original"
-    improvement_val = pd.DataFrame()
-    for method in df_pivot_val.columns:
-        if method == baseline_col:
-            continue
-        improvement = (df_pivot_val[baseline_col] - df_pivot_val[method]) / df_pivot_val[baseline_col] * 100
-        improvement_val[method] = improvement
 
-    avg_improvement_val = improvement_val.mean().sort_values(ascending=False)
+    #Plot
+    plot_autogluon_score_graph(dataset_list_wrapped, df_pivot_val, "Val")
+    plot_autogluon_score_graph(dataset_list_wrapped, df_pivot_test, "Test")
 
-    # ----- Plot average improvement -----
-    plt.figure(figsize=(10, 6))
-    # avg_improvement_val.plot(kind="bar", color="skyblue")
-    bars = avg_improvement_val.plot(kind="bar", color="skyblue")
-    for i, val in enumerate(avg_improvement_val):
-        y = -1 if val >= 0 else 0  # adjust offset for spacing
-        plt.text(i, y, f"{val:.2f}%", ha='center', va='top' if val >= 0 else 'bottom', color='black')
-    plt.axhline(0, color="black", linewidth=0.8)
-    plt.title("Average % Improvement Val over original Dataset')")
-    plt.xlabel("Method")
-    plt.ylabel("Average Improvement (%)")
-    plt.xticks(rotation=45, ha="right")
-    plt.grid(True, linestyle="--", alpha=0.6)
-    plt.tight_layout()
-    plt.savefig("test_results/Average_Percentage_Improvement_Val.png")
-    plt.show()
+    plot_random_vs_me_graph(dataset_list_wrapped, df_pivot_val, "Val")
+    plot_random_vs_me_graph(dataset_list_wrapped, df_pivot_test, "Test")
 
-    improvement_test = pd.DataFrame()
-    for method in df_pivot_val.columns:
-        if method == baseline_col:
-            continue
-        improvement = (df_pivot_test[baseline_col] - df_pivot_test[method]) / df_pivot_test[baseline_col] * 100
-        improvement_test[method] = improvement
+    plot_count_best(df_pivot_val, df_pivot_test, "")
+    plot_avg_perc_impr(baseline_col, df_pivot_val, "Val")
+    plot_avg_perc_impr(baseline_col, df_pivot_test, "Test")
 
-    avg_improvement_test = improvement_test.mean().sort_values(ascending=False)
-
-    plt.figure(figsize=(10, 6))
-    # avg_improvement_test.plot(kind="bar", color="skyblue")
-    bars = avg_improvement_test.plot(kind="bar", color="skyblue")
-    for i, val in enumerate(avg_improvement_test):
-        y = -1 if val >= 0 else 0  # adjust offset for spacing
-        plt.text(i, y, f"{val:.2f}%", ha='center', va='top' if val >= 0 else 'bottom', color='black')
-    plt.axhline(0, color="black", linewidth=0.8)
-    plt.title("Average % Improvement Test over original Dataset")
-    plt.xlabel("Method")
-    plt.ylabel("Average Improvement (%)")
-    plt.xticks(rotation=45, ha="right")
-    plt.grid(True, linestyle="--", alpha=0.6)
-    plt.tight_layout()
-    plt.savefig("test_results/Average_Percentage_Improvement_Test.png")
-    plt.show()
-
-
-    plt.figure(figsize=(12, 6))
-    for method in df_pivot_val.columns:
-        if method == "Best Random":
-            plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
-        elif method.startswith("pandas"):
-            plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
-        elif method.startswith("d2v"):
-            plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
-        elif method.startswith("MFE"):
-            plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
-        elif method.startswith("Original"):
-            plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
-
-    plt.xlabel("Dataset ID")
-    plt.xticks(rotation=45)  # or 90
-    plt.ylabel("Autogluon Val Score (flipped)")
-    plt.title("Autogluon Val Score by FE Method per Dataset (Best random vs. our method)")
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-    plt.savefig("test_results/Random_vs_Me_Val_graph.png")
-    plt.show()
-
-    plt.figure(figsize=(12, 6))
-    for method in df_pivot_val.columns:
-        if method == "Best Random":
-            plt.plot(dataset_list_wrapped, df_pivot_test[method], marker='o', label=method)
-        elif method.startswith("pandas"):
-            plt.plot(dataset_list_wrapped, df_pivot_test[method], marker='o', label=method)
-        elif method.startswith("d2v"):
-            plt.plot(dataset_list_wrapped, df_pivot_test[method], marker='o', label=method)
-        elif method.startswith("MFE"):
-            plt.plot(dataset_list_wrapped, df_pivot_test[method], marker='o', label=method)
-        elif method.startswith("Original"):
-            plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
-
-    plt.xlabel("Dataset ID")
-    plt.xticks(rotation=45)  # or 90
-    plt.ylabel("Autogluon Test Score (flipped)")
-    plt.title("Autogluon Test Score by FE Method per Dataset (Best random vs. our method)")
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-    plt.savefig("test_results/Random_vs_Me_Test_graph.png")
-    plt.show()
-
-    minValueIndex_val = df_pivot_val.idxmin(axis=1).value_counts()
-    minValueIndex_test = df_pivot_val.idxmin(axis=1).value_counts()
-
-    # Plot
-    plt.figure(figsize=(10, 6))
-    minValueIndex_val.plot(kind='bar', color='skyblue', label='Count of best val result on number of datasets')
-    minValueIndex_test.plot(kind='bar',  width=0.3, color='darkblue', label='Count of best test result on number of datasets')
-    plt.legend()
-    plt.xlabel("Method")
-    plt.ylabel("Best result on number of datasets")
-    plt.title("Which method is the best?")
-    plt.xticks(rotation=45, ha="right")
-    plt.tight_layout()
-    plt.savefig("test_results/Count_Best_bar.png")
-    plt.show()
-
-
+    # Drop OpenFE column
     df_pivot_val.drop(columns=["OpenFE"], inplace=True)
     df_pivot_test.drop(columns=["OpenFE"], inplace=True)
-    minValueIndex_val = df_pivot_val.idxmin(axis=1).value_counts()
-    minValueIndex_test = df_pivot_test.idxmin(axis=1).value_counts()
 
-    # Plot
-    plt.figure(figsize=(10, 6))
-    minValueIndex_val.plot(kind='bar', color='skyblue', label='Count of best val result on number of datasets')
-    minValueIndex_test.plot(kind='bar', width=0.3, color='darkblue', label='Count of best test result on number of datasets')
-    plt.legend()
-    plt.xlabel("Method")
-    plt.ylabel("Best result on number of datasets")
-    plt.title("Which method is the best?")
-    plt.xticks(rotation=45, ha="right")
-    plt.tight_layout()
-    plt.savefig("test_results/Count_Best_without_OpenFE_bar.png")
-    plt.show()
+    #Plot again
+    plot_count_best(df_pivot_val, df_pivot_test,"without_OpenFE")
+    plot_avg_perc_impr(baseline_col, df_pivot_val, "Val_without_OpenFE")
+    plot_avg_perc_impr(baseline_col, df_pivot_test, "Test_without_OpenFE")
 
-    # ----- Compute percentage improvement over baseline -----
-    baseline_col = "Original"
-    improvement_val = pd.DataFrame()
-    for method in df_pivot_val.columns:
-        if method == baseline_col:
-            continue
-        improvement = (df_pivot_val[baseline_col] - df_pivot_val[method]) / df_pivot_val[baseline_col] * 100
-        improvement_val[method] = improvement
-
-    avg_improvement_val = improvement_val.mean().sort_values(ascending=False)
-
-    # ----- Plot average improvement -----
-    plt.figure(figsize=(10, 6))
-    # avg_improvement_val.plot(kind="bar", color="skyblue")
-    bars = avg_improvement_val.plot(kind="bar", color="skyblue")
-    for i, val in enumerate(avg_improvement_val):
-        y = -1 if val >= 0 else 0  # adjust offset for spacing
-        plt.text(i, y, f"{val:.2f}%", ha='center', va='top' if val >= 0 else 'bottom', color='black')
-    plt.axhline(0, color="black", linewidth=0.8)
-    plt.title("Average % Improvement Val over original Dataset')")
-    plt.xlabel("Method")
-    plt.ylabel("Average Improvement (%)")
-    plt.xticks(rotation=45, ha="right")
-    plt.grid(True, linestyle="--", alpha=0.6)
-    plt.tight_layout()
-    plt.savefig("test_results/Average_Percentage_Improvement_Val_without_OpenFE.png")
-    plt.show()
-
-    improvement_test = pd.DataFrame()
-    for method in df_pivot_val.columns:
-        if method == baseline_col:
-            continue
-        improvement = (df_pivot_test[baseline_col] - df_pivot_test[method]) / df_pivot_test[baseline_col] * 100
-        improvement_test[method] = improvement
-
-    avg_improvement_test = improvement_test.mean().sort_values(ascending=False)
-
-    #for i, val in enumerate(avg_improvement_test):
-    #    plt.text(i, val + (1 if val >= 0 else -1), f"{val:.2f}%", ha='center', va='bottom' if val >= 0 else 'top')
-
-    plt.figure(figsize=(10, 6))
-    # avg_improvement_test.plot(kind="bar", color="skyblue")
-    bars = avg_improvement_test.plot(kind="bar", color="skyblue")
-    for i, val in enumerate(avg_improvement_test):
-        y = -1 if val >= 0 else 0  # adjust offset for spacing
-        plt.text(i, y, f"{val:.2f}%", ha='center', va='top' if val >= 0 else 'bottom', color='black')
-    plt.axhline(0, color="black", linewidth=0.8)
-    plt.title("Average % Improvement Test over original Dataset")
-    plt.xlabel("Method")
-    plt.ylabel("Average Improvement (%)")
-    plt.xticks(rotation=45, ha="right")
-    plt.grid(True, linestyle="--", alpha=0.6)
-    plt.tight_layout()
-    plt.savefig("test_results/Average_Percentage_Improvement_Test_without_OpenFE.png")
-    plt.show()
 
 if __name__ == "__main__":
     analyse_results()
