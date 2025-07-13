689f00d2fb59794ab8e8bb82ee1851c1fc0f280a
bastis007 <schaefer.bastian@mail.de>
2025-07-13 14:21:56 +0200

    Add incremental validation analysis

--- src/Result_Analysis/statistical_analysis/statistical_analysis.py
+++ src/Result_Analysis/statistical_analysis/statistical_analysis.py
@@ -1,11 +1,9 @@
 import glob
-import os
 
 import matplotlib.pyplot as plt
 import numpy as np
 import openml
 import pandas as pd
-from numpy.linalg import LinAlgError
 
 from src.Result_Analysis.test_analysis.test_analysis import insert_line_breaks
 from src.utils.get_data import split_data, get_openml_dataset_split
@@ -19,59 +17,73 @@ def plot_delta_r2_graph(dataset_list_wrapped, df_pivot, name):
     plt.xticks(rotation=45)  # or 90
     plt.yscale("log")
     plt.ylabel("DeltaR2 Score")
-    plt.title("DeltaR2 Score by FE Method per Dataset")
+    plt.title("DeltaR2 Score " + name + " by FE Method per Dataset")
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
-    plt.savefig("Delta_R2_Score_by_FE_Method"+name+".png")
+    if name == "":
+        plt.savefig("Delta_R2_Score_by_FE_Method.png")
+    else:
+        plt.savefig("Delta_R2_Score_by_FE_Method_" + name.replace(" ", "_") + ".png")
     plt.show()
 
 
+"""
 def semipartial_corr_formula(X_base, X_new, Y):
-    """
-    Compute semipartial correlation for each feature in X_new with Y,
-    controlling for X_base.
-    """
+    # Compute semipartial correlation for each feature in X_new with Y, controlling for X_base.
     X_base = np.asarray(X_base)
     Y = np.asarray(Y).ravel()
     results = {}
-
     for col in X_new.columns:
         x_new = X_new[col].to_numpy().ravel()
-
         # Correlation between Y and new feature
         r_yx1 = np.corrcoef(Y, x_new)[0, 1]
-
         # Correlation between Y and base features (multiple)
         r_yx2 = np.mean([np.corrcoef(Y, X_base[:, i])[0, 1] for i in range(X_base.shape[1])])
-
         # Correlation between new feature and base features (average)
         r_x1x2 = np.mean([np.corrcoef(x_new, X_base[:, i])[0, 1] for i in range(X_base.shape[1])])
-
         # Semipartial correlation
         numerator = r_yx1 - r_yx2 * r_x1x2
         denominator = np.sqrt(1 - r_x1x2 ** 2)
-
         r_semipartial = numerator / denominator if denominator != 0 else np.nan
         results[col] = r_semipartial
-
     return results
+"""
+
+
+def identify_suppressors(X_base, X_train_fe, Y, corr_matrix_pd, delta_r2, threshold_low=0.1, threshold_high=0.3):
+    suppressors = []
+
+    for col in X_train_fe.columns:
+        r_yx = corr_matrix_pd.loc[col, "target"]  # correlation with Y
+        r_xx = corr_matrix_pd.loc[col, X_base.columns].abs().max()  # strongest correlation with any X_base
+
+        if abs(r_yx) < threshold_low and r_xx > threshold_high:
+            suppressors.append({
+                'feature': col,
+                'r_with_Y': r_yx,
+                'max_r_with_X_base': r_xx,
+                'delta_r2': delta_r2
+            })
+
+    return pd.DataFrame(suppressors)
 
 
 def calculate_incremental_validity(Y, X_base, X_train_fe):
-
-    all_vars = pd.concat([X_train_fe, Y], axis=1)
+    Y_df = pd.DataFrame(Y, columns=["target"])
+    all_vars = pd.concat([X_train_fe, Y_df], axis=1)
     try:
         corr_matrix = all_vars.corr().values
     except ValueError:
         Y = pd.Series(pd.factorize(Y)[0], index=X_base.index)
-        all_vars = pd.concat([X_train_fe, Y], axis=1)
+        Y_df = pd.DataFrame(Y, columns=["target"])
+        all_vars = pd.concat([X_train_fe, Y_df], axis=1)
         try:
             corr_matrix = all_vars.corr().values
         except ValueError:
             X_base = X_base.apply(lambda x: pd.factorize(x)[0])
             X_train_fe = X_train_fe.apply(lambda x: pd.factorize(x)[0])
-            all_vars = pd.concat([X_train_fe, Y], axis=1)
+            all_vars = pd.concat([X_train_fe, Y_df], axis=1)
             corr_matrix = all_vars.corr().values
     corr_matrix = np.nan_to_num(corr_matrix)
     corr_matrix_pd = pd.DataFrame(corr_matrix, columns=all_vars.columns, index=all_vars.columns)
@@ -88,7 +100,6 @@ def calculate_incremental_validity(Y, X_base, X_train_fe):
     r2_full = r_yx_full @ np.linalg.pinv(R_xx_full) @ r_yx_full.T
 
     delta_r2 = r2_full - r2_base
-    print(delta_r2)
     return corr_matrix_pd, r2_base, r2_full, delta_r2
 
 
@@ -117,8 +128,11 @@ def statistical_analysis():
     path = "../../Apply_and_Test/test_data/"
     data_files = glob.glob(path + "FE_*.parquet")
     data_files.sort()
-    columns = ["Dataset", "Method", "DeltaR2"]
+    columns = ["Dataset", "Method", "DeltaR2", "Count_Suppressor_Candidates", "Number_of_Features", "DeltaR2_Normalized"]
     delta_r2_df = pd.DataFrame(columns=columns)
+    delta_r2_normalized_df = pd.DataFrame(columns=columns)
+    delta_r2_non_suppressor_df = pd.DataFrame(columns=columns)
+    delta_r2_non_suppressor_normalized_df = pd.DataFrame(columns=columns)
     try:
         delta_r2_df = pd.read_parquet("delta_r2.parquet")
     except FileNotFoundError:
@@ -130,20 +144,54 @@ def statistical_analysis():
             X_train_fe, y_train_fe, X_test_fe, y_test_fe = split_data(data, "target")
             X_train, y_train, X_test, y_test = get_openml_dataset_split(dataset)
             features = X_train_fe.drop(X_train.columns, axis=1)
-            X_train = X_train_fe.drop(features.columns, axis=1)
-            y_train = y_train_fe
-            X_test = X_test_fe.drop(features.columns, axis=1)
-            y_test = y_test_fe
-            corr_matrix_pd, r2_base, r2_full, delta_r2 = calculate_incremental_validity(y_train_fe, X_train, X_train_fe)
+            number_of_features = features.shape[1]
+            if number_of_features > 0:
+                X_train = X_train_fe.drop(features.columns, axis=1)
+                corr_matrix_pd, r2_base, r2_full, delta_r2 = calculate_incremental_validity(y_train_fe, X_train, X_train_fe)
+                suppressor_df = identify_suppressors(X_train, X_train_fe, y_train_fe, corr_matrix_pd, delta_r2)
+                delta_r2_normalized = delta_r2 / number_of_features
+                count_suppressor_candidates = len(suppressor_df)
+                try:
+                    X_train_fe_non_suppressors = X_train_fe.drop(columns=suppressor_df['feature'])
+                except KeyError:
+                    X_train_fe_non_suppressors = X_train_fe
+                corr_matrix_pd_non_suppressor, r2_base_non_suppressor, r2_full_non_suppressor, delta_r2_non_suppressor = calculate_incremental_validity(y_train_fe, X_train, X_train_fe_non_suppressors)
+                delta_r2_non_suppressor_normalized = delta_r2_non_suppressor / number_of_features
+            else:
+                delta_r2 = 0
+                delta_r2_normalized = 0
+                delta_r2_non_suppressor = 0
+                delta_r2_non_suppressor_normalized = 0
+                count_suppressor_candidates = 0
+            print(number_of_features)
+            print(delta_r2)
+            print(delta_r2_normalized)
+            print(delta_r2_non_suppressor)
+            print(delta_r2_non_suppressor_normalized)
+
+            # Suppressor & normalized
             new_row = pd.DataFrame(columns=columns)
-            new_row.loc[len(new_row)] = [dataset, method, delta_r2,]
+            new_row.loc[len(new_row)] = [dataset, method, delta_r2, count_suppressor_candidates, number_of_features, delta_r2_normalized]
             delta_r2_df = pd.concat([delta_r2_df, new_row])
 
-        delta_r2_df.dropna(inplace=True)
-        delta_r2_df.to_parquet("delta_r2.parquet")
+            # No suppressor & normalized
+            new_row_non_suppressor = pd.DataFrame(columns=columns)
+            new_row_non_suppressor.loc[len(new_row_non_suppressor)] = [dataset, method, delta_r2_non_suppressor, count_suppressor_candidates, number_of_features, delta_r2_non_suppressor_normalized]
+            delta_r2_non_suppressor_df = pd.concat([delta_r2_non_suppressor_df, new_row_non_suppressor])
+
+            delta_r2_df.dropna(inplace=True)
+            delta_r2_df.to_parquet("delta_r2.parquet")
+            delta_r2_non_suppressor_df.dropna(inplace=True)
+            delta_r2_non_suppressor_df.to_parquet("delta_r2_non_suppressor.parquet")
 
     df_pivot = delta_r2_df.pivot(index="Dataset", columns="Method", values="DeltaR2")
-    df_pivot = df_pivot.sort_index()  # Sort by dataset ID
+    df_pivot_normalized = delta_r2_df.pivot(index="Dataset", columns="Method", values="DeltaR2_Normalized")
+    df_pivot_non_suppressor = delta_r2_non_suppressor_df.pivot(index="Dataset", columns="Method", values="Count_Suppressor_Candidates")
+
+    df_pivot = df_pivot.sort_index()
+    df_pivot_normalized = df_pivot_normalized.sort_index()
+    df_pivot_non_suppressor = df_pivot_non_suppressor.sort_index()
+
     datasets = df_pivot.index.astype(str)
     dataset_list = []
     for dataset in datasets.tolist():
@@ -159,10 +207,13 @@ def statistical_analysis():
     dataset_list_wrapped = [insert_line_breaks(name, max_len=15) for name in dataset_list]
 
     plot_delta_r2_graph(dataset_list_wrapped, df_pivot, "")
+    plot_delta_r2_graph(dataset_list_wrapped, df_pivot_normalized, "Normalized")
 
     df_pivot_pandas = df_pivot[["pandas_CatBoost_best", "pandas_CatBoost_recursion", "OpenFE"]]
-    plot_delta_r2_graph(dataset_list_wrapped, df_pivot_pandas, "_pandas_OpenFE_")
+    df_pivot_normalized_pandas = df_pivot_normalized[["pandas_CatBoost_best", "pandas_CatBoost_recursion", "OpenFE"]]
 
+    plot_delta_r2_graph(dataset_list_wrapped, df_pivot_pandas, "Pandas and OpenFE")
+    plot_delta_r2_graph(dataset_list_wrapped, df_pivot_normalized_pandas, "Normalized Pandas and OpenFE")
 
 
 
