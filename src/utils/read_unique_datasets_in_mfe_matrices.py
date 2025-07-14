eb65470ce38c8b2686acc589245411cf527effe8
bastis007 <schaefer.bastian@mail.de>
2025-07-14 11:35:10 +0200

    Minor changes

--- src/utils/read_unique_datasets_in_mfe_matrices.py
+++ src/utils/read_unique_datasets_in_mfe_matrices.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-files = ["src/Metadata/core/Core_Matrix_Complete.parquet", "src/Metadata/d2v/D2V_Matrix_Complete.parquet", "src/Metadata/pandas/Pandas_Matrix_Complete.parquet", "src/Metadata/mfe/MFE_General_Matrix_Complete.parquet"]
+files = ["../Metadata/core/Core_Matrix_Complete.parquet", "../Metadata/d2v/D2V_Matrix_Complete.parquet", "../Metadata/pandas/Pandas_Matrix_Complete.parquet", "../Metadata/mfe/MFE_General_Matrix_Complete.parquet"]
 
 for file in files:
     dataset = pd.read_parquet(file)
