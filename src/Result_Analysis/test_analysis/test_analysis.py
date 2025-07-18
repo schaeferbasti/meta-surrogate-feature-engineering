4f8cd2f32c59c24c0338e014a06e374c1da75244
bastis007 <schaefer.bastian@mail.de>
2025-07-18 12:03:39 +0200

    Rename and refactor plots

--- src/Result_Analysis/test_analysis/test_analysis.py
+++ src/Result_Analysis/test_analysis/test_analysis.py
@@ -43,56 +43,41 @@ def get_data(result_files):
         )
         dataset = task.get_dataset().name
         dataset_list.append(dataset)
-    dataset_list_wrapped = [insert_line_breaks(name, max_len=15) for name in dataset_list]
+    #dataset_list_wrapped = [insert_line_breaks(name, max_len=15) for name in dataset_list]
+    dataset_list_wrapped = datasets.tolist()
     return dataset_list_wrapped, df_pivot_test, df_pivot_val
 
 
-def plot_autogluon_score_graph(dataset_list_wrapped, df_pivot_val, name):
-    if "only_pandas" in name:
+def plot_score_graph(dataset_list_wrapped, df_pivot, name):
+    if "only_pandas" in name or "openfe_pandas" in name:
         score_type = name.split("_")[0]
         if score_type == "Val":
-            score_type = "Validation"
+            score_type = "validation"
+        else:
+            score_type = "test"
+    elif "without_openfe" in name:
+        score_type = name.split("_")[0]
+        if score_type == "Val":
+            score_type = "validation"
+        else:
+            score_type = "test"
+        df_pivot = df_pivot.drop(["OpenFE"], axis=1)
     else:
-        score_type = "Test"
-    plt.figure(figsize=(12, 6))
-    for method in df_pivot_val.columns:
-        plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
-    plt.xlabel("Dataset ID")
-    plt.xticks(rotation=90)  # or 90
-    plt.ylabel(score_type + " Score")
-    plt.title(score_type + " score of the model on the feature-engineered datasets, the original and the randomly feature-engineered datasets")
+        if name == "Val":
+            score_type = "validation"
+        else:
+            score_type = "test"
+    plt.figure(figsize=(12, 10))
+    for method in df_pivot.columns:
+        plt.plot(dataset_list_wrapped, df_pivot[method], marker='o', label=method)
+    plt.xlabel("Dataset")
+    plt.xticks(rotation=90)  # or 45
+    plt.ylabel(score_type.title() + " Score")
+    plt.title(score_type.title() + " score of the model on the feature-engineered datasets, the original and the randomly feature-engineered datasets")
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
-    plt.savefig("../Result_Analysis/test_analysis/Autogluon_" + name + "_Score_by_FE_Method.png")
-    plt.show()
-
-
-def plot_random_vs_me_graph(dataset_list_wrapped, df_pivot_val, name):
-    if name == "Val":
-        score_type = "Validation"
-    else:
-        score_type = "Test"
-    plt.figure(figsize=(12, 6))
-    for method in df_pivot_val.columns:
-        if method == "Best Random":
-            plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
-        elif method.startswith("pandas"):
-            plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
-        elif method.startswith("d2v"):
-            plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
-        elif method.startswith("MFE"):
-            plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
-        elif method.startswith("Original"):
-            plt.plot(dataset_list_wrapped, df_pivot_val[method], marker='o', label=method)
-    plt.xlabel("Dataset ID")
-    plt.xticks(rotation=90)  # or 90
-    plt.ylabel(score_type + " Score")
-    plt.title(score_type + " score of the model on the feature-engineered datasets, the original and the randomly feature-engineered datasets")
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-    plt.savefig("../Result_Analysis/test_analysis/Random_vs_Me_" + name + "_graph.png")
+    plt.savefig("../Result_Analysis/test_analysis/Graph_" + name + ".png")
     plt.show()
 
 
@@ -100,7 +85,7 @@ def plot_count_best(df_pivot_val, df_pivot_test, name):
     minValueIndex_val = df_pivot_val.idxmin(axis=1).value_counts()
     minValueIndex_test = df_pivot_test.idxmin(axis=1).value_counts()
     # Plot
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(12, 10))
     minValueIndex_val.plot(kind='bar', color='skyblue', label='Count of the best validation scores over all datasets')
     minValueIndex_test.plot(kind='bar', width=0.3, color='darkblue',
                             label='Count of the best test scores over all datasets')
@@ -116,9 +101,9 @@ def plot_count_best(df_pivot_val, df_pivot_test, name):
 
 def plot_avg_percentage_impr(baseline_col, df_pivot, name, only_pandas=False):
     if name == "Val":
-        score_type = "Validation"
+        score_type = "validation"
     else:
-        score_type = "Test"
+        score_type = "test"
     improvement = pd.DataFrame()
     for method in df_pivot.columns:
         if method == baseline_col:
@@ -131,7 +116,7 @@ def plot_avg_percentage_impr(baseline_col, df_pivot, name, only_pandas=False):
     avg_improvement = improvement.mean().sort_values(ascending=False)
     # for i, val in enumerate(avg_improvement_test):
     #    plt.text(i, val + (1 if val >= 0 else -1), f"{val:.2f}%", ha='center', va='bottom' if val >= 0 else 'top')
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(12, 10))
     # avg_improvement_test.plot(kind="bar", color="skyblue")
     bars = avg_improvement.plot(kind="bar", color="skyblue")
     if only_pandas:
@@ -155,9 +140,9 @@ def plot_avg_percentage_impr(baseline_col, df_pivot, name, only_pandas=False):
 
 def plot_boxplot_percentage_impr(baseline_col, df_pivot, name):
     if name == "Val":
-        score_type = "Validation"
+        score_type = "validation"
     else:
-        score_type = "Test"
+        score_type = "test"
     improvement_test = pd.DataFrame()
     for method in df_pivot.columns:
         if method == baseline_col:
@@ -170,7 +155,7 @@ def plot_boxplot_percentage_impr(baseline_col, df_pivot, name):
     improvement_test = improvement_test[method_order]
 
     # Plot
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(12, 10))
     improvement_test.boxplot(column=method_order, grid=True)
     plt.axhline(0, color="black", linewidth=0.8, linestyle="--")
     plt.title("Distribution of the percentage loss reduction of the " + score_type + " score of the model\nin relation to the " + score_type + " score of the model on the original datasets")
@@ -188,11 +173,11 @@ def test_analysis():
     dataset_list_wrapped, df_pivot_test, df_pivot_val = get_data(result_files)
 
     # Plot
-    plot_autogluon_score_graph(dataset_list_wrapped, df_pivot_val, "Val")
-    plot_autogluon_score_graph(dataset_list_wrapped, df_pivot_test, "Test")
+    plot_score_graph(dataset_list_wrapped, df_pivot_val, "Val")
+    plot_score_graph(dataset_list_wrapped, df_pivot_test, "Test")
 
-    plot_random_vs_me_graph(dataset_list_wrapped, df_pivot_val, "Val")
-    plot_random_vs_me_graph(dataset_list_wrapped, df_pivot_test, "Test")
+    plot_score_graph(dataset_list_wrapped, df_pivot_val, "Val_without_openfe")
+    plot_score_graph(dataset_list_wrapped, df_pivot_test, "Test_without_openfe")
 
     plot_count_best(df_pivot_val, df_pivot_test, "")
     plot_avg_percentage_impr(baseline_col, df_pivot_val, "Val")
@@ -201,31 +186,51 @@ def test_analysis():
     plot_boxplot_percentage_impr(baseline_col, df_pivot_test, "Test")
 
     # Drop OpenFE column to compare MFE approaches
-    df_pivot_val.drop(columns=["OpenFE"], inplace=True)
-    df_pivot_test.drop(columns=["OpenFE"], inplace=True)
+    df_pivot_val_without_openfe = df_pivot_val
+    df_pivot_test_without_openfe = df_pivot_test
+    df_pivot_val_without_openfe.drop(columns=["OpenFE"], inplace=True)
+    df_pivot_test_without_openfe.drop(columns=["OpenFE"], inplace=True)
     # Plot again
-    plot_count_best(df_pivot_val, df_pivot_test, "without_OpenFE_")
-    plot_avg_percentage_impr(baseline_col, df_pivot_val, "Val_without_OpenFE")
-    plot_avg_percentage_impr(baseline_col, df_pivot_test, "Test_without_OpenFE")
-    plot_boxplot_percentage_impr(baseline_col, df_pivot_val, "Val_without_OpenFE")
-    plot_boxplot_percentage_impr(baseline_col, df_pivot_test, "Test_without_OpenFE")
+    plot_count_best(df_pivot_val_without_openfe, df_pivot_test_without_openfe, "without_OpenFE_")
+    plot_avg_percentage_impr(baseline_col, df_pivot_val_without_openfe, "Val_without_OpenFE")
+    plot_avg_percentage_impr(baseline_col, df_pivot_test_without_openfe, "Test_without_OpenFE")
+    plot_boxplot_percentage_impr(baseline_col, df_pivot_val_without_openfe, "Val_without_OpenFE")
+    plot_boxplot_percentage_impr(baseline_col, df_pivot_test_without_openfe, "Test_without_OpenFE")
 
     # Drop everything but pandas & original columns to compare SM approaches
-    df_pivot_val = df_pivot_val[["pandas_best", "pandas_recursion", "Original"]]
-    df_pivot_test = df_pivot_test[["pandas_best", "pandas_recursion", "Original"]]
+    df_pivot_val_pandas = df_pivot_val[["pandas_best", "pandas_recursion", "Original"]]
+    df_pivot_test_pandas = df_pivot_test[["pandas_best", "pandas_recursion", "Original"]]
     # Plot
-    plot_avg_percentage_impr(baseline_col, df_pivot_val, "Val_only_pandas", True)
-    plot_avg_percentage_impr(baseline_col, df_pivot_test, "Test_only_pandas", True)
-    plot_boxplot_percentage_impr(baseline_col, df_pivot_val, "Val_only_pandas")
-    plot_boxplot_percentage_impr(baseline_col, df_pivot_test, "Test_only_pandas")
+    plot_avg_percentage_impr(baseline_col, df_pivot_val_pandas, "Val_only_pandas", True)
+    plot_avg_percentage_impr(baseline_col, df_pivot_test_pandas, "Test_only_pandas", True)
+    plot_boxplot_percentage_impr(baseline_col, df_pivot_val_pandas, "Val_only_pandas")
+    plot_boxplot_percentage_impr(baseline_col, df_pivot_test_pandas, "Test_only_pandas")
 
     # Drop everything but pandas columns to compare SM approaches
-    df_pivot_val = df_pivot_val[["pandas_best", "pandas_recursion"]]
-    df_pivot_test = df_pivot_test[["pandas_best", "pandas_recursion"]]
+    df_pivot_val_pandas = df_pivot_val_pandas[["pandas_best", "pandas_recursion"]]
+    df_pivot_test_pandas = df_pivot_test_pandas[["pandas_best", "pandas_recursion"]]
     # Plot again
-    plot_count_best(df_pivot_val, df_pivot_test, "only_pandas_")
-    plot_autogluon_score_graph(dataset_list_wrapped, df_pivot_val, "Val_only_pandas")
-    plot_autogluon_score_graph(dataset_list_wrapped, df_pivot_test, "Test_only_pandas")
+    plot_count_best(df_pivot_val_pandas, df_pivot_test_pandas, "only_pandas_")
+    plot_score_graph(dataset_list_wrapped, df_pivot_val_pandas, "Val_only_pandas")
+    plot_score_graph(dataset_list_wrapped, df_pivot_test_pandas, "Test_only_pandas")
+
+    dataset_list_wrapped, df_pivot_test, df_pivot_val = get_data(result_files)
+
+    df_pivot_val_openfe = df_pivot_val[["OpenFE", "pandas_recursion", "Original"]]
+    df_pivot_test_openfe = df_pivot_test[["OpenFE", "pandas_recursion", "Original"]]
+    # Plot
+    plot_avg_percentage_impr(baseline_col, df_pivot_val_openfe, "Val_openfe_pandas", True)
+    plot_avg_percentage_impr(baseline_col, df_pivot_test_openfe, "Test_openfe_pandas", True)
+    plot_boxplot_percentage_impr(baseline_col, df_pivot_val_openfe, "Val_openfe_pandas")
+    plot_boxplot_percentage_impr(baseline_col, df_pivot_test_openfe, "Test_openfe_pandas")
+
+    # Drop everything but pandas columns to compare SM approaches
+    df_pivot_val_openfe = df_pivot_val_openfe[["OpenFE", "pandas_recursion"]]
+    df_pivot_test_openfe = df_pivot_test_openfe[["OpenFE", "pandas_recursion"]]
+    # Plot again
+    plot_count_best(df_pivot_val_openfe, df_pivot_test_openfe, "openfe_pandas_")
+    plot_score_graph(dataset_list_wrapped, df_pivot_val_openfe, "Val_openfe_pandas")
+    plot_score_graph(dataset_list_wrapped, df_pivot_test_openfe, "Test_openfe_pandas")
 
 
 if __name__ == "__main__":
