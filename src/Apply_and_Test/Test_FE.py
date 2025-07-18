ac57d18b09c109cd9004b512edbbe2ca80adc1ba
bastis007 <schaefer.bastian@mail.de>
2025-07-18 12:04:04 +0200

    Fix bug with OpenFE test_data files

--- src/Apply_and_Test/Test_FE.py
+++ src/Apply_and_Test/Test_FE.py
@@ -79,6 +79,7 @@ def main():
             continue
 
 
+
         combined_results = [original_results, openfe_results]
 
         # === METHOD RESULTS (Random/pandas/MFE/d2v) ===
@@ -86,44 +87,47 @@ def main():
         best_score = float('-inf')
 
         for data_file in files:
-            print(f"  Processing file: {data_file}")
-            name = data_file.split(f'FE_{dataset_id}_')[1]
-
-            if "fold" in name:
-                fold = name.split("fold_")[1].split(".")[0]
-                method = name.split('_')[0] + f"_{fold}"
-                is_random = True
-                result_path = f"test_results/{method}_Result_{dataset_id}.parquet"
-            elif "MFE" in name:
-                version = name.split('.parquet')[0].split("_")[-1]
-                category = name.split("MFE_")[1].split("_")[0]
-                method = name.split('_')[0] + f"_{category}_{version}"
-                is_random = False  # e.g., pandas
-                result_path = f"test_results/{method}_Result_{dataset_id}.parquet"
+            if "OpenFE" in data_file:
+                print("Skip")
             else:
-                version = name.split('.parquet')[0].split("_")[-1]
-                method = name.split('_')[0] + "_" + version
-                is_random = False  # e.g., pandas
-                result_path = f"test_results/{method}_Result_{dataset_id}.parquet"
-            try:
-                results = pd.read_parquet(result_path)
-            except (FileNotFoundError, pyarrow.lib.ArrowInvalid):
-                df = pd.read_parquet(data_file)
-                Xf_train, yf_train, Xf_test, yf_test = split_data(df, target_label)
-                if task_type == "Supervised Classification":
-                    results = get_model_score_origin_classification(Xf_train, yf_train, Xf_test, yf_test, dataset_id, method)
+                print(f"  Processing file: {data_file}")
+                name = data_file.split(f'FE_{dataset_id}_')[1]
+
+                if "fold" in name:
+                    fold = name.split("fold_")[1].split(".")[0]
+                    method = name.split('_')[0] + f"_{fold}"
+                    is_random = True
+                    result_path = f"test_results/{method}_Result_{dataset_id}.parquet"
+                elif "MFE" in name:
+                    version = name.split('.parquet')[0].split("_")[-1]
+                    category = name.split("MFE_")[1].split("_")[0]
+                    method = name.split('_')[0] + f"_{category}_{version}"
+                    is_random = False  # e.g., pandas
+                    result_path = f"test_results/{method}_Result_{dataset_id}.parquet"
                 else:
-                    results = get_model_score_origin_regression(Xf_train, yf_train, Xf_test, yf_test, dataset_id, method)
-                results = results[results['model'] == "LightGBM_BAG_L1"]
-                results.to_parquet(result_path)
+                    version = name.split('.parquet')[0].split("_")[-1]
+                    method = name.split('_')[0] + "_" + version
+                    is_random = False  # e.g., pandas
+                    result_path = f"test_results/{method}_Result_{dataset_id}.parquet"
+                try:
+                    results = pd.read_parquet(result_path)
+                except (FileNotFoundError, pyarrow.lib.ArrowInvalid):
+                    df = pd.read_parquet(data_file)
+                    Xf_train, yf_train, Xf_test, yf_test = split_data(df, target_label)
+                    if task_type == "Supervised Classification":
+                        results = get_model_score_origin_classification(Xf_train, yf_train, Xf_test, yf_test, dataset_id, method)
+                    else:
+                        results = get_model_score_origin_regression(Xf_train, yf_train, Xf_test, yf_test, dataset_id, method)
+                    results = results[results['model'] == "LightGBM_BAG_L1"]
+                    results.to_parquet(result_path)
 
-            if is_random:
-                score = results["score_test"].values[0]
-                if score > best_score:
-                    best_score = score
-                    best_random_result = results
-            else:
-                combined_results.append(results)  # Only non-random (e.g. pandas)
+                if is_random:
+                    score = results["score_test"].values[0]
+                    if score > best_score:
+                        best_score = score
+                        best_random_result = results
+                else:
+                    combined_results.append(results)  # Only non-random (e.g. pandas)
 
             # Append the best random result
         if best_random_result is not None:
